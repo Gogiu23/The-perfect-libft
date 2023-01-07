d0b76ec4a9299d82bd4110e76a87a4d81406e836
Gogiu23 <giuliandominici@gmail.com>
2023-01-07 19:58:01 +0100

    libft

--- sources/lines/get_next_line_utils_bonus.c
+++ sources/lines/get_next_line_utils_bonus.c
@@ -5,20 +5,13 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: gdominic <gdominic@student.42barcelona.co  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/07/05 16:36:54 by gdominic          #+#    #+#             */
-<<<<<<< HEAD
-/*   Updated: 2023/01/05 23:33:39 by gdominic         ###   ########.fr       */
-=======
-/*   Updated: 2022/12/16 00:26:54 by gdominic         ###   ########.fr       */
->>>>>>> origin/Start
+/*   Created: 2023/01/07 14:19:10 by gdominic          #+#    #+#             */
+/*   Updated: 2023/01/07 14:19:14 by gdominic         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
 #include "../../includes/libft.h"
 
-<<<<<<< HEAD
-
-=======
->>>>>>> origin/Start
 size_t	ft2_strlen(char *str)
 {
 	size_t	count;
